c0977439b7f13f1b27b7ab04e2f80c4d99efccc6
Edilberto Firmino <edilberto_ti@yahoo.com.br>
2025-09-25 08:12:24 -0300

    criacao do arquivo git

---
+++ aulagithub.sh
@@ -0,0 +1,11 @@
+#olhar o caderno do amigo 
+git branch #para ver qual é a branch
+
+#para comecar a trabalhar criando minha propria branch
+git checkout -b "nome da branch"
+
+#ja fez tudo que tinha para fazer, ai voce sobe para o github
+git status #para ver o status
+git add . #mandar tudo que foi feito
+git commit -m "eu fiz uma melhoria"
+git push origin Edilberto #subir para o repositorio
